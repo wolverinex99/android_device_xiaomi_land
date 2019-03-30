2e7dc3d1f4e517a598d81a7e2989df295e608860
Ritesh Saxena <riteshsax007@gmail.com>
2019-03-30 04:11:51 +0000

    land: Update blobs from TA-1024
    Change-Id: Ieaebee35bd7942fbf3c3f5ec30950f71c5e880ee

--- device.mk
+++ device.mk
@@ -125,6 +125,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbt-vendor
 
+# Boost Framework
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    UxPerformance
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8937
